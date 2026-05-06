75905895c1b74de422d20a69d06a66b5dd465963
rupesh07-ai <rupeshdabil17@gmail.com>
2026-05-06 15:17:43 +0530

    update

---
+++ LECTURE2/Fibonacci.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n):
+
+        a = 0
+        b = 1
+
+        for i in range(n):
+            a, b = b, a + b
+
+        return a
---
+++ LECTURE2/recursivestack.py
@@ -0,0 +1,8 @@
+def fun(n):
+    if n == 0:
+        return
+    fun(n-1)
+    print(n, end = ' ')
+    
+fun(3)
+    
