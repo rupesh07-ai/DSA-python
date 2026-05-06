2418fa9f4b4d617dec10dc58dda53b8273021120
rupesh07-ai <rupeshdabil17@gmail.com>
2026-05-06 13:15:51 +0530

    First commit

--- L2.py
+++ L2.py
@@ -1,15 +1,6 @@
 n = 10
 
-#for i in range(n//2):
- #   print(i, end =' ')
+for i in range(n//2):
+    print(i, end =' ')
     
     
-#for i in range(n+1000):
- #   print(i, end=' ')
-    
-    
-#for i in range(n*1000):
- #   print(i, end=' ')    
- 
-for i in range(n**2):
-    print(i)
